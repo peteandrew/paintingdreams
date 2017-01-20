dddd6e3d04d53d3973263fc59678c51d69d25536
Pete Andrew <peteandrew101@gmail.com>
2017-01-20 18:03:53 +0000

    Added minimum order quantity on A3 prints

--- paintingdreams/wholesale/forms.py
+++ paintingdreams/wholesale/forms.py
@@ -49,6 +49,7 @@ def products_table():
 def info():
     html = """
         <p class="wholesale-order-info">Cards have a minimum order quantity of 3.</p>
+        <p class="wholesale-order-info">A3 prints have a minimum order quantity of 2.</p>
         <p class="wholesale-order-info">The minimum order amount is £50.</p>
         {% if postage_option == 'std' %}<p class="postage-info">For orders under £125 there is a delivery charge of £8. For orders of £125 and over there is no delivery charge.</p><p class="postage-info">For international orders, postage will be calculated depending on the weight of the order.</p>{% endif %}
         {% if postage_option == 'wghtcalc' %}<p class="postage-info">The carriage cost will be calculated depending on weight and you will be notified of the total cost before dispatch. Please confirm you are happy to proceed with the order once you have received the notification.</p>{% endif %}"""
