b9c1c5cd40757a3fe72b938372ff8786e077226b
Pete Andrew <peteandrew101@gmail.com>
2017-02-08 23:42:28 +0000

    Changed postage band calculation to include rather than exclude max weights

--- paintingdreams/mainapp/postage_prices.py
+++ paintingdreams/mainapp/postage_prices.py
@@ -7,7 +7,7 @@ def calculate(destination, weight):
     prices = PostagePrice.objects.filter(
         destination = destination,
         min_weight__lte = weight).filter(
-            Q(max_weight__exact = None) | Q(max_weight__gt = weight))
+            Q(max_weight__exact = None) | Q(max_weight__gte = weight))
 
     if len(prices) > 0:
         return prices[0].price
