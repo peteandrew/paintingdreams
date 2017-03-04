07fb869eeb1513d7f02ea1b4ca68c5dac299ff57
Pete Andrew <peteandrew101@gmail.com>
2017-03-04 17:39:27 +0000

    Changed product menu order. Hide galleries text on image detail when no galleries

--- paintingdreams/mainapp/models.py
+++ paintingdreams/mainapp/models.py
@@ -225,6 +225,9 @@ class ProductTag(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['name',]
+
     def __str__(self):
         return self.name
 
