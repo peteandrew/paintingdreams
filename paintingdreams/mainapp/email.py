1ec52ae960badd2b4ad2ea6d56a5ba17fd8459eb
Pete Andrew <peteandrew101@gmail.com>
2017-04-15 12:01:06 +0100

    Added html order emails

--- paintingdreams/mainapp/email.py
+++ paintingdreams/mainapp/email.py
@@ -5,30 +5,35 @@ from django.template.loader import render_to_string
 
 def send_order_complete_email(order):
     msg_plain_admin = render_to_string('email/transaction-complete-admin.txt', {'order': order})
+    msg_html_admin = render_to_string('email/transaction-complete-admin.html', {'order': order})
     msg_plain_customer = render_to_string('email/transaction-complete-customer.txt', {'order': order})
-    #msg_html = render_to_string('templates/email.html', {'some_params': some_params})
+    msg_html_customer = render_to_string('email/transaction-complete-customer.html', {'order': order})
 
     # Mail site orders administrator
     mail.send_mail(
-        'Order received',
-        msg_plain_admin,
-        settings.ORDERS_FROM_EMAIL,
-        [settings.ORDERS_ADMIN_EMAIL])
+        subject='Order received',
+        message=msg_plain_admin,
+        from_email=settings.ORDERS_FROM_EMAIL,
+        recipient_list=[settings.ORDERS_ADMIN_EMAIL],
+        html_message=msg_html_admin)
 
     # Mail customer
     mail.send_mail(
-        'Painting Dreams Order',
-        msg_plain_customer,
-        settings.ORDERS_FROM_EMAIL,
-        [order.customer_email])
+        subject='Painting Dreams Order',
+        message=msg_plain_customer,
+        from_email=settings.ORDERS_FROM_EMAIL,
+        recipient_list=[order.customer_email],
+        html_message=msg_html_customer)
 
 
 def send_payment_failed_email(order):
     msg_plain_customer = render_to_string('email/transaction-failed-customer.txt', {'order': order, 'site_base_url': settings.BASE_URL})
+    msg_html_customer = render_to_string('email/transaction-failed-customer.html', {'order': order, 'site_base_url': settings.BASE_URL})
 
     # Mail customer
     mail.send_mail(
-        'Painting Dreams Order Payment Failed',
-        msg_plain_customer,
-        settings.ORDERS_FROM_EMAIL,
-        [order.customer_email])
+        subject='Painting Dreams Order Payment Failed',
+        message=msg_plain_customer,
+        from_email=settings.ORDERS_FROM_EMAIL,
+        recipient_list=[order.customer_email],
+        html_message=msg_html_customer)
